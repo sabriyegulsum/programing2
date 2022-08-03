14c4ef7bd30797be7c40841d45b6b3775a7a5e55
sabriyegulsum <soylasabriye@icloud.com>
2022-08-03 17:34:37 +0300

    .

--- lab4/question7.c
+++ lab4/question7.c
@@ -2,15 +2,23 @@
 int main()
 {
     int i=0,j;
-    char s[]={"sabriye gulsum soylu"};
+    char s[100];
     char *p;
+    gets(s);
     p=s;
-    while(i!=)
+    printf("%s",p);
+    while(*(p+i)!='\0') // NULL yazınca kod çalışmıyo ama olur böyle şeyler
     {
-        
         i++;
     }
     for(j=i;j>=0;j--)
-        printf("%c",*p+i);
+    {
+        printf("%c",*(p+j));
+    }
+
+    printf("\n%d",i);
     return 0;
 }
+/*önemli not: bir dizinin bütün elemanlarını sadece adını yazdırdığında  hepsini yazdıramazsın 
+ama stringde bunu yapabilirsin bu stringin özelliğidir. pointer da eiştlediğine göre uyumludur 
+I mean  eğer bir pointer stringi tutarsa sadece pointerın adıyla hepsinni yazabilirsin ama dizide olmaz
