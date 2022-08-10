7d066b9ce3411c7eb23dd305be947911ddbd780a
sabriyegulsum <soylasabriye@icloud.com>
2022-08-10 11:18:06 +0300

    .

---
+++ c_examples/pointer/respectivvely_connecting_two_array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//anağğ bu nedir yaaa :)) boş bir satıra bir saatimi verdim
+int main()
+{
+    char *name,isim[8]="sabriye";
+    int sum=0,no=2010213051,i,j;
+    name=(char *)malloc(8*sizeof(char));
+    strcpy(name,isim);
+    while(no!=0)
+    {
+        sum+=no%10;
+        no=no/10;
+    }
+    i=7;
+    name=realloc(name,sum*sizeof(char));
+    while(sum>7)
+    {
+        for(j=0;j<7;j++)
+            *(name+i+j)=*(isim+j);
+        i+=7;
+        sum-=7;
+    }
+    printf("%s",name);
+    return 0;
+}
