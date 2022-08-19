b7dd27215edd2c5a46859748d7244e5d8d02dfbc
sabriyegulsum <soylasabriye@icloud.com>
2022-08-19 12:32:10 +0300

    .

--- week8/struct_deneme_twin.c
+++ week8/struct_deneme_twin.c
@@ -10,6 +10,7 @@ int main()
     printf("enter she birthday day month year");
     scanf("%d %d %d",&she.day,&she.month,&she.year);
     her_twin=she;
+    // struct değişkenlerinide birbirine eşitleyebiliriz. aynı x ve y leri eşitleidiğimz gibi değerlerini diğerine kopyalar
     printf("her twin birth day:%d %d %d",her_twin.day,her_twin.month,her_twin.year);
 
     return 0;
